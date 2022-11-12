43ba1142124543fc257db54ff3ebbfe6dee4b4b9
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2022-11-12 20:11:16 +0600

    add skeleton ui library

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { AppBar, AppShell } from '@brainandbones/skeleton';
+	import '@brainandbones/skeleton/styles/all.css';
+	import '@brainandbones/skeleton/themes/theme-skeleton.css';
+	import '../app.postcss';
+</script>
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<h1>Skeleton</h1>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn btn-sm" href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</nav>
+	</svelte:fragment>
+	<slot />
+</AppShell>
